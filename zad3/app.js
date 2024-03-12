825293e046a7a7a0016bfbbfa4a1b326884f1f99
Barteeeekkk <wnorowski918@gmail.com>
2024-03-13 00:01:50 +0100

    update2

---
+++ zad3/app.js
@@ -0,0 +1,7 @@
+const student = module.export = {
+    getStudentFullName,
+    getStudentId,
+};
+
+
+console.log(`My name is ${student.getStudentFullName}. My student ID is ${student.getStudentId}`);
--- zad3/student.js
+++ zad3/student.js
@@ -9,4 +9,3 @@ function getStudentId(){
     return STUDENT_ID;
 }
 
-console.log(`My name is ${FULLNAME}. My student ID is ${STUDENT_ID}`);
